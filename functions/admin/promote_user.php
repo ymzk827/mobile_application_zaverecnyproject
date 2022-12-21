5ac4fc47ce07ca52b6cd6a0649aa33e2e97e4ece
ymzk827 <emuzyka827@gmail.com>
2022-12-21 11:51:39 +0100

    minor fix

--- functions/admin/promote_user.php
+++ functions/admin/promote_user.php
@@ -1,29 +1,30 @@
 <?php
-// Definovanie koreňovej cesty (dva priečinky nad aktuálnym súborom)
 define('__ROOT__', dirname(dirname(__DIR__)));
 require_once(__ROOT__ . '/functions/config.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = intval($_POST['id']);
 
-    // Predpokladáme, že $config je definovaný v config.php
     $conn = new mysqli($config["HOST"], $config["USER_NAME"], $config["PASSWORD"], 'zaverecny_project');
-
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $stmt = $conn->prepare("UPDATE user SET role = 'admin' WHERE id = ?");
+    $stmt = $conn->prepare("UPDATE user SET rola = 'admin' WHERE id = ?");
     if (!$stmt) {
         die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
     }
 
     $stmt->bind_param("i", $id);
 
-    
+    if ($stmt->execute()) {
+        echo "Success";
+    } else {
+        echo "Promote failed: " . $stmt->error;
+    }
+
     $stmt->close();
     $conn->close();
-    // header("Location: ".__ROOT__ ."/admin.php");
     exit;
 } else {
     echo "Invalid request.";
