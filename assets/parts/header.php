139c1afa8c7bacb2f09f1661dbbdfa99efa28509
ymzk827 <emuzyka827@gmail.com>
2025-03-17 20:16:07 +0100

    separated footer and header

---
+++ assets/parts/header.php
@@ -0,0 +1,29 @@
+<header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              UXOS
+            </span>
+          </a>
+
+          <div class="navbar-collapse" id="">
+            <div class="custom_menu-btn">
+              <button onclick="openNav()">
+                <span class="s-1"> </span>
+                <span class="s-2"> </span>
+                <span class="s-3"> </span>
+              </button>
+            </div>
+            <div id="myNav" class="overlay">
+              <div class="overlay-content">
+                <a href="index.php">HOME</a>
+                <a href="about.php">ABOUT</a>
+                <a href="feature.php">FEATURE</a>
+                <a href="contact.php">CONTACT US</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
